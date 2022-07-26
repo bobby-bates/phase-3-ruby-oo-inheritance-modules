187cfba70e2c655e521de9d70cc0b8b0988b33f6
Bobby Bates <robertfbatesjr@gmail.com>
2022-07-26 18:26:15 -0400

    Complete assignment

--- lib/dancer.rb
+++ lib/dancer.rb
@@ -1,2 +1,20 @@
 require_relative './dance_module'
 require_relative './meta_dancing_module'
+require_relative './fancy_dance'
+
+class Dancer
+  # # lend this class all of the class methods of the MetaDancing module
+  # extend MetaDancing
+  # # Lend this class all of the instance methods of the Dance module
+  # include Dance
+
+  # Refactor:
+  extend FancyDance::ClassMethods
+  include FancyDance::InstanceMethods
+
+  attr_accessor :name
+
+  def initialize name
+    @name = name
+  end
+end
